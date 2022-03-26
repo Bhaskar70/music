232c13c9613c486f94277400e2d43913dbf15595
Bhaskar70 <82130189+Bhaskar70@users.noreply.github.com>
2022-03-26 09:14:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,38 +16,38 @@ function musicPlay(key) {
     switch (key) {
         case "w":
             var tom1 = new Audio();
-            tom1.src = "sounds/tom-1.mp3"
+            tom1.src = "tom-1.mp3"
             tom1.play();
             break;
         case "a":
             var audio2 = new Audio();
-            audio2.src = "sounds/kick-bass.mp3"
+            audio2.src = "kick-bass.mp3"
             audio2.play();
             break;
         case "s":
             var tom3 = new Audio();
-            tom3.src = "sounds/tom-3.mp3"
+            tom3.src = "tom-3.mp3"
             tom3.play();
             break;
         case "d":
             var crash = new Audio();
-            crash.src = "sounds/crash.mp3"
+            crash.src = "crash.mp3"
             crash.play();
             break;
         case "j":
             var tom2 = new Audio();
-            tom2.src = "sounds/tom-2.mp3"
+            tom2.src = "tom-2.mp3"
             tom2.play();
             break;
         case "k":
             var snare = new Audio();
-            snare.src = "sounds/snare.mp3"
+            snare.src = "snare.mp3"
             snare.play();
 
             break;
         case "l":
             var tom4 = new Audio();
-            tom4.src = "sounds/tom-4.mp3"
+            tom4.src = "tom-4.mp3"
             tom4.play();
             break;
             deafault: console.log("hello")
